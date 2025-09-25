0487ccda1a956bc0be9f60c67ebf5c4ac988f392
netrebnic <70092629+roblanc@users.noreply.github.com>
2025-09-25 23:15:35 +0300

    tom

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -72,7 +72,7 @@ export const DATA = {
 
   work: [
     {
-      company: "Atomic Finance",
+      company: "Consultant",
       href: "https://atomic.finance",
       badges: [],
       location: "Remote",
@@ -84,65 +84,29 @@ export const DATA = {
         "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
     },
     {
-      company: "Shopify",
+      company: "PTC",
       badges: [],
-      href: "https://shopify.com",
-      location: "Remote",
-      title: "Software Engineer",
+      href: "https://ptc.com",
+      location: "Bucharest, RO",
+      title: "Software Marketing Global Specialist",
       logoUrl: "/shopify.svg",
-      start: "January 2021",
-      end: "April 2021",
+      start: "January 2016",
+      end: "April 2020",
       description:
         "Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.",
     },
     {
-      company: "Nvidia",
-      href: "https://nvidia.com/",
+      company: "PTC",
+      href: "https://ptc.com/",
       badges: [],
-      location: "Santa Clara, CA",
+      location: "Bucharest, RO",
       title: "Software Engineer",
       logoUrl: "/nvidia.png",
       start: "January 2020",
       end: "April 2020",
       description:
         "Architected and wrote the entire MVP of the GeForce Now Cloud Gaming internal admin and A/B testing dashboard using React, Redux, TypeScript, and Python.",
-    },
-    {
-      company: "Splunk",
-      href: "https://splunk.com",
-      badges: [],
-      location: "San Jose, CA",
-      title: "Software Engineer",
-      logoUrl: "/splunk.svg",
-      start: "January 2019",
-      end: "April 2019",
-      description:
-        "Co-developed a prototype iOS app with another intern in Swift for the new Splunk Phantom security orchestration product (later publicly demoed and launched at .conf annual conference in Las Vegas). Implemented a realtime service for the iOS app in Django (Python) and C++; serialized data using protobufs transmitted over gRPC resulting in an approximate 500% increase in data throughput.",
-    },
-    {
-      company: "Lime",
-      href: "https://li.me/",
-      badges: [],
-      location: "San Francisco, CA",
-      title: "Software Engineer",
-      logoUrl: "/lime.svg",
-      start: "January 2018",
-      end: "April 2018",
-      description:
-        "Proposed and implemented an internal ruby API for sending/receiving commands to scooters over LTE networks. Developed a fully automated bike firmware update system to handle asynchronous firmware updates of over 100,000+ scooters worldwide, and provide progress reports in real-time using React, Ruby on Rails, PostgreSQL and AWS EC2 saving hundreds of developer hours.",
-    },
-    {
-      company: "Mitre Media",
-      href: "https://mitremedia.com/",
-      badges: [],
-      location: "Toronto, ON",
-      title: "Software Engineer",
-      logoUrl: "/mitremedia.png",
-      start: "May 2017",
-      end: "August 2017",
-      description:
-        "Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener",
-    },
+
   ],
   education: [
     {
@@ -154,12 +118,12 @@ export const DATA = {
       end: "2024",
     },
     {
-      school: "University of Waterloo",
-      href: "https://uwaterloo.ca",
-      degree: "Bachelor's Degree of Computer Science (BCS)",
+      school: "University of Bucharest",
+      href: "https://www.fabiz.ase.ro/",
+      degree: "Master's Degree in Business Administration in Foreign Languages",
       logoUrl: "/waterloo.png",
       start: "2016",
-      end: "2021",
+      end: "2019",
     },
     {
       school: "Wilfrid Laurier University",
