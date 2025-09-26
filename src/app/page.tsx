8a3c5afefbbfbf180f1f08d1aa61f56975f0370c
netrebnic <70092629+roblanc@users.noreply.github.com>
2025-09-26 22:23:41 +0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -203,10 +203,10 @@ export default function Page() {
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                  I also make videos
+                  I make video essays
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  I also make videos
+                  I make video essays
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   I create videos about technology, philosophy, and culture. Here are some of my favorites.
--- src/data/videos.ts
+++ src/data/videos.ts
@@ -1,11 +1,5 @@
 export const VIDEOS = [
   {
-    id: "wV0w2h_Tq3E",
-  },
-  {
-    id: "mgAnCuhyKiM",
-  },
-  {
     id: "--67klZ-cNI",
   },
 ];
