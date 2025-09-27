f38762d5d2a68687f0216e760fb930cf225d93d1
netrebnic <70092629+roblanc@users.noreply.github.com>
2025-09-27 20:27:09 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -159,7 +159,7 @@ export default function Page() {
           <BlurFade delay={BLUR_FADE_DELAY * 13}>
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-<h4 className="text-3xl font-bold tracking-tighter sm:text-5xl">I read a lot of books</h4>
+<h4 className="text-3xl font-bold tracking-tighter sm:text-5xl">Book reviews</h4>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   I enjoy reading books and sharing my thoughts on them. Here are some of my book reviews.
                 </p>
