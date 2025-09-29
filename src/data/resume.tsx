16857c2936066b6169d494a235783f136bd1b978
netrebnic <70092629+roblanc@users.noreply.github.com>
2025-09-29 11:15:15 +0300

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -106,7 +106,7 @@ export const DATA = {
       start: "2015",
       end: "20016",
       description:
-        "Architected and wrote the entire MVP of the GeForce Now Cloud Gaming internal admin and A/B testing dashboard using React, Redux, TypeScript, and Python.",
+        "During my internship at PTC Eastern Europe, I supported the marketing and partner enablement teams by assisting with digital campaigns, content creation, and event coordination. I helped organize webinars and workshops, contributed to drafting internal and external communications, and gained hands-on experience with tools like Salesforce and LMS platforms. The role gave me early exposure to how global marketing operations work in practice, from managing partner communications to supporting technical content delivery, and it laid the foundation for my later full-time role at PTC.",
     },
   ],
   education: [
